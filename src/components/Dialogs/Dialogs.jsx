0ac9c7ac57bb11655efe2743f211140f0acd5c18
Andrey <parrayop@gmail.com>
2021-07-14 14:25:12 +0300

    feat: Add "lesson 21" features

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,20 @@
+import s from './Dialogs.module.css'
+
 const Dialogs = () =>
-    <div>
-        Dialogs
+    <div className={s.dialogs}>
+        <div className={s.dialogItems}>
+            <div className={`${s.dialog} ${s.active}`}>Andrey</div>
+            <div className={s.dialog}>Masha</div>
+            <div className={s.dialog}>Sasha</div>
+            <div className={s.dialog}>Misha</div>
+            <div className={s.dialog}>Jenny</div>
+            <div className={s.dialog}>Ivan</div>
+        </div>
+        <div className={s.messages}>
+            <div className={s.message}>Hi</div>
+            <div className={s.message}>How are you</div>
+            <div className={s.message}>Yo</div>
+        </div>
     </div>
 
 export default Dialogs
