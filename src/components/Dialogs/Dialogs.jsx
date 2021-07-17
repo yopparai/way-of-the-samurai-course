1cdddc354e0f38f9e855ea72739887ea2dc9cf8e
Andrey <parrayop@gmail.com>
2021-07-17 21:26:56 +0300

    feat: Add "lesson 31" homework - Update "Dialogs" component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,31 @@
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
     const dialogsElements = props.state.dialogs
         .map(dialog => <DialogItem id={dialog.id} name={dialog.name}/>)
     const messagesElements = props.state.messages
         .map(message => <Message message={message.message}/>)
+    const newMessageElement = React.createRef()
+    const addMessage = () => {
+        const text = newMessageElement.current.value
+        alert(text)
+    }
     return <div className={s.dialogs}>
         <div className={s.dialogItems}>
             {dialogsElements}
         </div>
         <div className={s.messages}>
-            {messagesElements}
+            <div>{messagesElements}</div>
+            <div>
+                <textarea ref={newMessageElement}/>
+            </div>
+            <div>
+                <button onClick={addMessage}>Add message
+                </button>
+            </div>
         </div>
     </div>
 }
