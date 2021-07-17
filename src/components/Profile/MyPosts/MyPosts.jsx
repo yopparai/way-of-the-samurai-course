87e58738674b55bdc7768c8f5d7927942ca5e6f5
Andrey <parrayop@gmail.com>
2021-07-17 13:10:53 +0300

    feat: Add "lesson 31" features

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,24 @@
 import Post from "./Post/Post";
 import s from './MyPosts.module.css'
+import React from "react";
 
 const MyPosts = (props) => {
     const postsElements = props.posts
         .map(post => <Post message={post.message} likeCount={post.likesCount}/>)
+    const newPostElement = React.createRef()
+    const addPost = () => {
+        const text = newPostElement.current.value
+        alert(text)
+    }
     return <div className={s.postBlock}>
         <h3>My posts</h3>
         <div>
             <div>
-                <textarea/>
+                <textarea ref={newPostElement}/>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post
+                </button>
             </div>
         </div>
         <div className={s.posts}>
