05e0bac70c0423561fad240e835860d159ab4554
yopparai <parrayop@gmail.com>
2021-07-24 17:34:16 +0300

    feat: Add "lesson 37" features

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import './index.css';
-import state, {addMessage, addPost, subscribe, updateNewMessageText, updateNewPostText} from "./redux/state";
+import store from "./redux/store";
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
 
-export const renderEntireTree = (state) =>
+const renderEntireTree = (state) =>
     ReactDOM.render(
         <React.StrictMode>
             <App state={state}
-                 addPost={addPost}
-                 updateNewPostText={updateNewPostText}
-                 addMessage={addMessage}
-                 updateNewMessageText={updateNewMessageText}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 
-renderEntireTree(state)
+renderEntireTree(store.getState())
 
-subscribe(renderEntireTree)
+store.subscribe(renderEntireTree)
---
+++ src/redux/store.js
@@ -0,0 +1,74 @@
+const store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hello!', likesCount: 24},
+                {id: 2, message: 'How are you?', likesCount: 10},
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Andrey'},
+                {id: 2, name: 'Masha'},
+                {id: 3, name: 'Sasha'},
+                {id: 4, name: 'Misha'},
+                {id: 5, name: 'Jenny'},
+                {id: 6, name: 'Ivan'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you'},
+                {id: 3, message: 'Yo'}
+            ],
+            newMessageText: ''
+        },
+        navbar: {
+            friends: [
+                {id: 1, name: 'Sergey', avatar: 'https://wallpaperaccess.com/full/2020287.jpg'},
+                {
+                    id: 2,
+                    name: 'Benjamin',
+                    avatar: 'https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509__340.jpg'
+                },
+                {id: 3, name: 'Fran', avatar: 'https://www.un.org/sites/un2.un.org/files/ourbeautifulocean-12x.png'}
+            ]
+        }
+    },
+    getState() {
+        return this._state
+    },
+    addPost() {
+        const newPost = {
+            id: 3,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newPostText) {
+        this._state.profilePage.newPostText = newPostText
+        this._callSubscriber(this._state)
+    },
+    addMessage() {
+        const newMessage = {
+            id: 4,
+            message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._state.dialogsPage.newMessageText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewMessageText(newMessageText) {
+        this._state.dialogsPage.newMessageText = newMessageText
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    }
+}
+
+
+export default store
