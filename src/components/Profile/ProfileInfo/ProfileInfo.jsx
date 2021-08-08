1d069dd66ccb41856b9303f7689a6d485b3c2852
yopparai <parrayop@gmail.com>
2021-08-08 13:49:11 +0300

    fix: Change profile image url

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,7 +3,7 @@ import s from './ProfileInfo.module.css'
 const ProfileInfo = () =>
     <div>
         <div>
-            <img src='https://wallpapercave.com/wp/wp4649492.jpg'/>
+            <img src='https://i.pinimg.com/originals/c1/50/f8/c150f806a08b037564541f249d3e3214.jpg'/>
         </div>
         <div className={s.descriptionBlock}>
             ava + description
