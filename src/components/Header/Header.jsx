49097c237081c7421f13846d0075093bd453c119
yopparai <parrayop@gmail.com>
2021-11-26 23:04:24 +0300

    feat: Add "lesson 61" features

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () =>
+const Header = (props) =>
     <header className={s.header}>
         <img src='https://www.strunkmedia.com/wp-content/uploads/2018/05/bigstock-221516158.jpg'/>
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 
 export default Header
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -3,12 +3,14 @@ import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import navbarReducer from "./navbarReducer";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 
 const reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     navbar: navbarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 })
 
 const store = createStore(reducers)
