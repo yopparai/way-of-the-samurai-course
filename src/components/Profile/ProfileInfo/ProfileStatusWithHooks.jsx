261952f940b3798bdc232284daf1005eb0cef27b
yopparai <parrayop@gmail.com>
2021-12-12 18:22:36 +0300

    feat: Add "lesson 85" features

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,8 +1,12 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const ProfileStatusWithHooks = (props) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const onStatusChange = (e) => {
         setStatus(e.currentTarget.value)
